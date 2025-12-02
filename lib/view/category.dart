a7cbd083f67d404055fb3690357890629719535d
Bashar Khedr <basharkhedr@Dr-Bashar-Khedr.local>
2025-12-02 15:06:57 +0200

    added some features

--- lib/view/category.dart
+++ lib/view/category.dart
@@ -1,3 +1,4 @@
+import 'package:cosmatics_app/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CategoryView extends StatelessWidget {
@@ -6,7 +7,16 @@ class CategoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Categories')),
+      appBar: AppBar(
+        title: Text(
+          'Categories',
+          style: TextStyle(
+            color: AppColors.headLineTextColor,
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(14),
         child: Column(
@@ -22,6 +32,17 @@ class CategoryView extends StatelessWidget {
                 ),
                 labelText: 'Search',
               ),
+            ),
+            SizedBox(height: 32,),
+            ListTile(trailing: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward_ios)),
+              leading: ClipRRect(borderRadius: BorderRadiusGeometry.circular(10),
+                child: SizedBox(
+                  child: Image.network(
+                    'https://cdn.pixabay.com/photo/2018/06/20/06/21/cosmetics-3485914_1280.jpg',
+                  ),
+                ),
+              ),
+              title: Text('Title', style: TextStyle(color: Colors.red)),
             ),
           ],
         ),
